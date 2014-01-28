061f3f8a3dbef93aeb298ae21bb1e757a5d6ab36
Daisuke Miyamoto <dai.0304@gmail.com>
2014-01-28 15:20:25 +0900

    add EC2 authorieze/revoke security group ingress/egress permission

---
+++ src/main/groovy/jp/classmethod/aws/gradle/ec2/AmazonEC2AuthorizeSecurityGroupEgressTask.groovy
@@ -0,0 +1,46 @@
+package jp.classmethod.aws.gradle.ec2
+
+import java.util.List
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.GradleException
+import org.gradle.api.tasks.TaskAction
+
+import com.amazonaws.AmazonClientException
+import com.amazonaws.AmazonServiceException
+import com.amazonaws.services.ec2.*
+import com.amazonaws.services.ec2.model.*
+
+
+class AmazonEC2AuthorizeSecurityGroupEgressTask extends AbstractAmazonEC2SecurityGroupPermissionTask {
+	
+	{
+		description 'Authorize security group ingress.'
+		group = 'AWS'
+	}
+	
+	String groupId
+	
+	def ipPermissions
+	
+	@TaskAction
+	def createApplication() {
+		if (! groupId) throw new GradleException("groupId is not specified")
+		if (! ipPermissions) throw new GradleException("ipPermissions is not specified")
+		
+		AmazonEC2PluginExtension ext = project.extensions.getByType(AmazonEC2PluginExtension)
+		AmazonEC2 ec2 = ext.ec2
+		
+		try {
+			ec2.authorizeSecurityGroupEgress(new AuthorizeSecurityGroupEgressRequest()
+				.withGroupId(groupId)
+				.withIpPermissions(parse(toCollection(ipPermissions))))
+		} catch (AmazonServiceException e) {
+			if (e.getErrorCode() == 'InvalidPermission.Duplicate') {
+				logger.warn(e.getMessage())
+			} else {
+				throw e
+			}
+		}
+	}
+}
