89a9296efa9c74174f3ed528977838f51c288429
Daisuke Miyamoto <dai.0304@gmail.com>
2014-01-28 17:27:50 +0900

    remote EnvironmentVariableCredentialsProvider from EnvironmentVariableCredentialsProvider.

--- src/main/groovy/jp/classmethod/aws/gradle/AwsPlugin.groovy
+++ src/main/groovy/jp/classmethod/aws/gradle/AwsPlugin.groovy
@@ -47,7 +47,6 @@ class AwsPluginExtension {
 	
 	def AWSCredentialsProvider newCredentialsProvider(String accessKeyId, String secretKey) {
 		return new AWSCredentialsProviderChain(
-			new EnvironmentVariableCredentialsProvider(),
 			new SystemPropertiesCredentialsProvider(),
 			new StaticCredentialsProvider((accessKeyId && secretKey) ?
 				new BasicAWSCredentials(accessKeyId, secretKey) : null),
