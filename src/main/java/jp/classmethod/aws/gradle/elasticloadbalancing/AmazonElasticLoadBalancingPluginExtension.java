e229a816fe0e7cd44ec9ab9919050fc50fa4db8a
Daisuke Miyamoto <dai.0304@gmail.com>
2015-05-21 13:57:01 +0900

    Javanize ELB tasks and plugin.

---
+++ src/main/java/jp/classmethod/aws/gradle/elasticloadbalancing/AmazonElasticLoadBalancingPluginExtension.java
@@ -0,0 +1,40 @@
+package jp.classmethod.aws.gradle.elasticloadbalancing;
+
+import jp.classmethod.aws.gradle.AwsPluginExtension;
+import lombok.Getter;
+import lombok.Setter;
+
+import org.gradle.api.Project;
+
+import com.amazonaws.regions.RegionUtils;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.elasticloadbalancing.AmazonElasticLoadBalancing;
+import com.amazonaws.services.elasticloadbalancing.AmazonElasticLoadBalancingClient;
+
+
+public class AmazonElasticLoadBalancingPluginExtension {
+	
+	public static final String NAME = "elb";
+	
+	@Getter @Setter
+	private	Project project;
+	
+	@Getter @Setter
+	private	String profileName;
+	
+	@Getter @Setter
+	private String region = Regions.US_EAST_1.getName();
+		
+	@Getter(lazy = true)
+	private final AmazonElasticLoadBalancing elb = initClient();
+
+	public AmazonElasticLoadBalancingPluginExtension(Project project) {
+		this.project = project;
+	}
+
+	private AmazonElasticLoadBalancing initClient() {
+		AwsPluginExtension aws = project.getExtensions().getByType(AwsPluginExtension.class);
+		return aws.createClient(AmazonElasticLoadBalancingClient.class, RegionUtils.getRegion(this.region), profileName);
+	}
+	
+}
