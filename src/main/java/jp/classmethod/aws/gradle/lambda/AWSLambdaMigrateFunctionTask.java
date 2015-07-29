32596fa0b4797f5cba1123b39db164a12e826228
Daisuke Miyamoto <dai.0304@gmail.com>
2015-07-29 11:20:55 +0900

    Improve logging

--- src/main/java/jp/classmethod/aws/gradle/lambda/AWSLambdaMigrateFunctionTask.java
+++ src/main/java/jp/classmethod/aws/gradle/lambda/AWSLambdaMigrateFunctionTask.java
@@ -93,6 +93,7 @@ public class AWSLambdaMigrateFunctionTask extends ConventionTask {
 			updateStack(lambda, getFunctionResult);
 		} catch (ResourceNotFoundException e) {
 			getLogger().warn(e.getMessage());
+			getLogger().warn("Creating function... {}", functionName);
 			createFunction(lambda);
 		}
 	}
